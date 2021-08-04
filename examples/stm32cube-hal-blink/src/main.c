d7ad6d118809da07c5e6150e68ad91a3a1cb3fd1
qitas <qitas@qitas.cn>
2021-08-04 10:50:15 +0800

    add

--- examples/stm32cube-hal-blink/src/main.c
+++ examples/stm32cube-hal-blink/src/main.c
@@ -1,33 +1,7 @@
-#if F0
-#include "stm32f0xx_hal.h"
-#elif F1
-#include "stm32f1xx_hal.h"
-#elif F2
-#include "stm32f2xx_hal.h"
-#elif F3
-#include "stm32f3xx_hal.h"
-#elif F4
-#include "stm32f4xx_hal.h"
-#elif F7
-#include "stm32f7xx_hal.h"
-#elif L0
-#include "stm32l0xx_hal.h"
-#elif L1
-#include "stm32l1xx_hal.h"
-#elif L4
-#include "stm32l4xx_hal.h"
-#else
-#error "Unsupported STM32 Family"
-#endif
+#include "main.h"
 
-#define LED_PIN                                GPIO_PIN_5
-#define LED_GPIO_PORT                          GPIOA
-#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
-
-int main(void)
+void LED_Init()
 {
-  HAL_Init();
-  
   LED_GPIO_CLK_ENABLE();
   
   GPIO_InitTypeDef GPIO_InitStruct;
@@ -35,13 +9,23 @@ int main(void)
   GPIO_InitStruct.Pin = LED_PIN;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
   HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct); 
+    
+}
+
+#ifndef UNIT_TEST
+int main(void)
+#else
+int app_main(void)
+#endif
+{
+  HAL_Init();
+  LED_Init();
 
   while (1)
   {
     HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
-    
     HAL_Delay(1000);
   }
 }
